16b2b988c5293f3d879704a5111c22bfea764f77
Chance Valentine <chancecardona@protonmail.com>
2024-12-05 02:30:00 -0700

    Update to use dask

--- warmup_1.py
+++ warmup_1.py
@@ -7,24 +7,30 @@ import dask.array as da
 import dask.bag as db
 
 # Init spark session
-spark = SparkSession.builder \
-    .master("local") \
-    .config("spark.sql.autoBroadcastJoinThreshold", -1) \
-    .config("spark.executor.memory", "500mb") \
-    .appName("Exercise1") \
-    .getOrCreate()
+#spark = SparkSession.builder \
+#    .master("local") \
+#    .config("spark.sql.autoBroadcastJoinThreshold", -1) \
+#    .config("spark.executor.memory", "500mb") \
+#    .appName("Exercise1") \
+#    .getOrCreate()
 
-products_table = spark.read.parquet("./data/products_parquet")
-sales_table = spark.read.parquet("./data/sales_parquet")
-sellers_table = spark.read.parquet("./data/sellers_parquet")
+products_table = dd.read_parquet("./data/products_parquet")
+sales_table = dd.read_parquet("./data/sales_parquet")
+sellers_table = dd.read_parquet("./data/sellers_parquet")
 
-print(f"Number of Orders: {sales_table.count()}")
-print(f"Number of Products: {products_table.count()}")
-print(f"Number of Sellers: {sellers_table.count()}")
+print(f"Number of Orders: {sales_table.count().compute()}")
+print(f"Number of Products: {products_table.count().compute()}")
+print(f"Number of Sellers: {sellers_table.count().compute()}")
 
-print(f"Number of products sold at least once: {sales_table.agg(countDistinct(col('product_id')))}")
+print(f"Number of products sold at least once: {sales_table.groupby('product_id').size().compute()}")
 #   Output which is the product that has been sold in more orders
 print("Product present in more orders")
-sales_table.groupBy(col("product_id")).agg(
-        count("*").alias("cnt")
-    ).orderBy(col("cnt").desc()).limit(1).show()
+result = (sales_table
+         .groupby("product_id")
+         .size() # .agg in pyspark. Counts rows in each group.
+         .reset_index()
+         .rename(columns={0: "cnt"}) # count column is renamed to cnt
+         .nlargest(1, "cnt")
+         )
+
+print(result.compute())
